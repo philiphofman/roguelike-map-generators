bcb5d4245a290e0f8948075520338190984a00fa
Philip Hofman <philip.hofman@gmail.com>
2021-02-22 01:50:18 -0800

    Add a create_room unit test to test_map.py

--- tests/unit/test_map.py
+++ tests/unit/test_map.py
@@ -1,4 +1,5 @@
 from map_objects.map import Map
+from map_objects.rectangle import Rect
 
 
 def test_class_initialization():
@@ -23,3 +24,28 @@ def test_class_initialization():
             assert tile.blocked == expected_blocked
             assert tile.block_sight == expected_block_sight
             assert tile.explored == expected_explored
+
+
+def test_create_room():
+    m1 = Map(10, 10)
+    room_x = 1
+    room_y = 1
+    room_w = 5
+    room_h = 5
+
+    test_room = Rect(room_x, room_y, room_w, room_h)
+
+    m1.create_room(test_room)
+
+    for x_count, x in enumerate(m1.tiles):
+        for y_count, y in enumerate(x):
+            if room_x < x_count < (room_x + room_w):
+                if room_y < y_count < (room_y + room_h):
+                    assert y.blocked is False
+                    assert y.block_sight is False
+                else:
+                    assert y.blocked is True
+                    assert y.block_sight is True
+            else:
+                assert y.blocked is True
+                assert y.block_sight is True
