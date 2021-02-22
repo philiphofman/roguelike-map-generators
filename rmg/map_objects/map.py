34ebe8bb251129435a11f978dd85eed338725389
Philip Hofman <philip.hofman@gmail.com>
2021-02-22 01:23:54 -0800

    Expand docstrings in map.py
    Adjusted class docstring to follow Google formatting and added
    more information to the initialize_tiles function docstring.

--- rmg/map_objects/map.py
+++ rmg/map_objects/map.py
@@ -2,7 +2,17 @@ from map_objects.tile import Tile
 
 
 class Map:
-    """Map stores Tiles in a 2D array."""
+    """A 2D array of Tile objects.
+
+    The Map class stores Tile objects in a 2D array to create a
+    basic top-down map. It also includes some helper functions to
+    streamline basic interactions such as creating rooms and corridors.
+
+    Attributes:
+        width (int): The int width (x) of the map.
+        height (int): The int height (y) of the map.
+        tiles (List): The 2D array of Tile objects.
+    """
 
     def __init__(self, width, height):
         """Inits values for the map.
@@ -19,7 +29,13 @@ class Map:
     def initialize_tiles(self):
         """Creates a 2D array of tiles with own width and height.
 
-        By default every tile blocks movement.
+        By default every tile blocks movement and sight. The 2D array
+        is a nested list, with x being the top level list, and y being
+        the nested list, like so:
+
+        x0 -> [[y0, y1, y2],
+        x1 ->  [y0, y1, y2],
+        x2 ->  [y0, y1, y2]]
 
         Returns:
             tiles(list): A 2D array of tiles with the dimensions of the GameMap
